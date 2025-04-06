33b8837006dc374d70547c80533e6ed6ca5668db
Hibaa <Hiba.alashraf90@gmail.com>
2025-04-05 21:41:37 -0400

    Initial commit with .gitignore to exclude .vs and other files

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ERMSProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
